8ef6524818d4f833d3b619e1e371fefae323e740
Cristian Camilo <kamiloreyVii@gmail.com>
2017-04-10 15:40:06 -0500

    Aplicacion en consola

---
+++ MyFirstWebServiceApplication/App_Code/Service.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+// [System.Web.Script.Services.ScriptService]
+
+public class Service : System.Web.Services.WebService
+{
+    public Service()
+    {
+
+
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+
+    {
+        return "Hola a todos";
+    }
+    [WebMethod]
+    public string Miprimermetodoweb(string PrimerNombre, string Apellido)
+    {
+        return string.Format("Bienvenido {0} {1}", PrimerNombre, Apellido);
+    }
+
+}
---
+++ MyFirstWebServiceConsumerApp/MyFirstWebServiceConsumerApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyFirstWebServiceConsumerApp.MyFirstWebServiceReference;
+namespace MyFirstWebServiceConsumerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service webService = new Service();
+            Console.WriteLine(webService.Miprimermetodoweb("Cristian", "Bejarano"));
+            Console.ReadLine();
+        }
+    }
+}
